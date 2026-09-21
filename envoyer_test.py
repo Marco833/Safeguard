cc1ba83d497437674299c404b3705259dda7fcee
Marc <spamou83@gmail.com>
2026-09-21 11:20:21 +0200

    envoyer_test.py generique : utilisateur detecte automatiquement

--- envoyer_test.py
+++ envoyer_test.py
@@ -1,10 +1,14 @@
 import smtplib
+import getpass
 from email.message import EmailMessage
+
+utilisateur = getpass.getuser()
+adresse = f"{utilisateur}@localhost"
 
 msg = EmailMessage()
 msg["Subject"] = "Recette"
-msg["From"] = "andre@localhost"
-msg["To"] = "andre@localhost"
+msg["From"] = adresse
+msg["To"] = adresse
 msg.set_content("Voici le fichier demandé")
 
 with open("recette_innocente.pdf", "rb") as f:
@@ -15,4 +19,4 @@ msg.add_attachment(contenu, maintype="application", subtype="pdf", filename="rec
 with smtplib.SMTP("localhost", 587) as smtp:
     smtp.send_message(msg)
 
-print("Mail envoyé.")
+print(f"Mail envoyé pour l'utilisateur : {utilisateur}")
